154d148d99cd3e0dcc9d3b3dcb257f7f80db2fbe
1PUSHIDO <interestingphysic@gmail.com>
2024-09-07 17:34:53 +0300

    IProductService interface for Magazine.Core added

---
+++ Magazine.Core/Services/IProductService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Magazine.Core.Models;
+
+namespace Magazine.Core.Services
+{
+    internal interface IProductService
+    {
+        Product Add(Product product)
+        {
+            //Adding...
+            return product;
+        }
+
+        Product Remove(Product product)
+        {
+            //Removing...
+            return product;
+        }
+
+        Product Edit(Product product)
+        {
+            //Editing...
+            return product;
+        }
+
+        Product Search(Product product)
+        {
+            //Searching...
+            return product;
+        }
+    }
+}
